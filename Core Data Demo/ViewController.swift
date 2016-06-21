d5e2e456999c72da2a273b174163af1df919659c
George G <ggogarten@gmail.com>
2016-06-21 11:38:46 -0400

    Create and Read entries from CoreData working

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+        newUser.setValue("George", forKey: "username")
+        
+        newUser.setValue("pass123", forKey: "password")
+        
+        do {
+            
+            try context.save()
+            
+        } catch {
+        
+            print("There was a problem")
+            
+        }
+        
+        
+        let request = NSFetchRequest(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.executeFetchRequest(request)
+            
+            print(results)
+            
+        } catch {
+            
+            print("Fetch Failed")
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
